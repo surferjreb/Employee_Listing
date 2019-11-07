e30dd8039dd527a7ded8fe33508342045429955e
captdread <jamesbrownre1@gmail.com>
2019-11-07 04:00:06 -0500

    Finished my Program.  Added the ability to write to a file.

--- src/com/brownj/Main.java
+++ src/com/brownj/Main.java
@@ -1,68 +1,121 @@
 package com.brownj;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     enum Profession{SCIENTIST, TEACHER, UNKNOWN}
-
-    class employeeRec{
-        private int id;
-        private char jobCode;
-        private String lastName;
-        private String firstName;
-        private String profession;
-
-        employeeRec(int id, char jobCode, String lastName, String firstName, String profession){
-            this.id = id;
-            this.jobCode = jobCode;
-            this.lastName = lastName;
-            this.firstName = firstName;
-            this.profession = profession;
-        }
-
-        public String getId() {
-            String idString = String.valueOf(id);
-            return idString;
-        }
-
-        public char getJob() {
-            return jobCode;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public String getProfession() {
-            return profession;
-        }
-    }
-
+    enum ScientistCodes{B, C, P}
 
     public static void main(String[] args) {
-	char[] scientistCodes = {'B', 'C', 'P'};
-	char[] teacherCodes = {'E', 'H', 'U'};
+    EmployeeRec myRec;
 	String[] lineElements;
+	ArrayList<EmployeeRec> records;
+	String id = "";
+	char jobCode = ' ';
+	String lastName = "";
+	String firstName= "";
+	String profession = "";
+
 
 	try{
         File file = new File("//home//captdread//JavaProgramming//Employee_Listing"+
                 "//employees.txt");
+        FileWriter fileWriter = new FileWriter("output.txt",true);
         Scanner fileIn = new Scanner(file);
-
+        records = new ArrayList<>();
 
         while(fileIn.hasNextLine()){
-            lineElements = fileIn.nextLine().split("\\s");
-            for(String value : lineElements){
-                System.out.println(value);
-            }
-        }
 
-    }
+            lineElements = fileIn.nextLine().split("\\s");
+
+            for(int i = 0; i < lineElements.length; i++){
+
+                switch(i){
+                    case 0:
+                            id = lineElements[i];
+                            break;
+
+                    case 1:
+                            jobCode = lineElements[i].charAt(0);
+                            break;
+
+                    case 2:
+                            lastName = lineElements[i];
+                            break;
+
+                    case 3:
+                            if(lineElements.length > 4){
+                                lastName = lastName + " " + lineElements[i];
+                                break;
+                            }
+
+                            firstName = lineElements[i];
+                            break;
+
+                    case 4:
+                            firstName = lineElements[i];
+                            break;
+                    default:
+                        System.out.println("Unable to process file");
+                        break;
+                }//end switch
+
+            }//end for
+
+            switch(jobCode){
+                case 'B':
+                        profession = String.valueOf(Profession.SCIENTIST);
+                        break;
+                case 'C':
+                        profession = String.valueOf(Profession.SCIENTIST);
+                        break;
+                case 'P':
+                        profession = String.valueOf(Profession.SCIENTIST);
+                        break;
+                case 'E':
+                        profession = String.valueOf(Profession.TEACHER);
+                        break;
+                case 'H':
+                        profession = String.valueOf(Profession.TEACHER);
+                        break;
+                case 'U':
+                        profession = String.valueOf(Profession.TEACHER);
+                        break;
+                default:
+                    profession = String.valueOf(Profession.UNKNOWN);
+                    break;
+            }
+            myRec = new EmployeeRec(id, jobCode,lastName, firstName, profession.toLowerCase());
+            records.add(myRec);
+        }//end while
+
+        fileIn.close();
+
+        for(int i = 0; i < records.size(); i++){
+            System.out.println("Id: " + records.get(i).getId());
+            fileWriter.write("Id: " + records.get(i).getId()+"\n");
+
+            System.out.println("Job Code: "+ records.get(i).getJob());
+            fileWriter.write("Job Code: "+ records.get(i).getJob()+"\n");
+
+            System.out.println("Last name: "+ records.get(i).getLastName());
+            fileWriter.write("Last name: "+ records.get(i).getLastName()+"\n");
+
+            System.out.println("First name: "+ records.get(i).getFirstName());
+            fileWriter.write("First name: "+ records.get(i).getFirstName()+"\n");
+
+            System.out.println("Profession: "+ records.get(i).getProfession());
+            fileWriter.write("Profession: "+ records.get(i).getProfession()+"\n");
+
+            System.out.println("\n");
+            fileWriter.write("\n");
+
+        }
+        fileWriter.close();
+    }//end try
 	catch(Exception e){
 	    e.printStackTrace();
     }
@@ -71,4 +124,41 @@ public class Main {
     }
 
 
+}
+
+class EmployeeRec{
+    private String id;
+    private char jobCode;
+    private String lastName;
+    private String firstName;
+    private String profession;
+
+    public EmployeeRec(String id, char jobCode, String lastName, String firstName, String profession){
+        this.id = id;
+        this.jobCode = jobCode;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.profession = profession;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public char getJob() {
+        return jobCode;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
 }
